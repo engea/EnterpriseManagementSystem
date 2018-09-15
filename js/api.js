29ac7b54e801ae8a752f8354b7ce959033ba9357
jiangwei <jiangwei@gagogroup.com>
2018-09-15 16:30:34 +0800

    delete kaoqin:

--- js/api.js
+++ js/api.js
@@ -5,95 +5,122 @@
 const host = "http://localhost:3000/api";
 
 function login(username, password) {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: `${host}/admin/login`, type: "POST", data: {username, password}, success: function (data) {
-                console.log(data.code);
-                if (data.code === 200) {
-                    resolve(data);
-                } else {
-                    reject(data);
-                }
-            }
-        });
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${host}/admin/login`, type: "POST", data: {username, password}, success: function (data) {
+        console.log(data.code);
+        if (data.code === 200) {
+          resolve(data);
+        } else {
+          reject(data);
+        }
+      }
     });
+  });
 }
 
 function addMember(data) {
-    return addData("/employeeInfos",data);
+  return addData("/employeeInfos", data);
 }
 
 function addZhiwei(data) {
-  return addData("/titleInfos",data);
+  return addData("/titleInfos", data);
+}
+
+function addPayroll(data) {
+  return addData("/payrollInfos", data);
 }
 
 function addDepartment(data) {
-    return addData("/departmentalInfos",data);
+  return addData("/departmentalInfos", data);
 }
 
 function addData(url, data) {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: `${host}${url}`, type: "POST", data: data, success: function (data) {
-                console.log(data.code);
-                if (data.code === 200) {
-                    resolve(data);
-                } else {
-                    reject(data);
-                }
-            }
-        });
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${host}${url}`, type: "POST", data: data, success: function (data) {
+        console.log(data.code);
+        if (data.code === 200) {
+          resolve(data);
+        } else {
+          reject(data);
+        }
+      }
     });
+  });
 }
 
 function delMemberById(id) {
-    return delDataById("/employeeInfos", id)
+  return delDataById("/employeeInfos", id)
 }
+
+function delPayrollById(id) {
+  return delDataById("/payrollInfos", id)
+}
+
 function delTitleById(id) {
   return delDataById("/titleInfos", id)
 }
 
 function delBumenById(id) {
-    return delDataById("/departmentalInfos", id)
+  return delDataById("/departmentalInfos", id)
 }
 
 function delDataById(url, id) {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: `${host}${url}/${id}`, type: "DELETE", success: function () {
-                resolve();
-            }, error: function () {
-                reject();
-            }
-        });
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${host}${url}/${id}`, type: "DELETE", success: function () {
+        resolve();
+      }, error: function () {
+        reject();
+      }
     });
+  });
 }
 
 function getMembers() {
-    return getData("/employeeInfos");
+  return getData("/employeeInfos");
 }
 
 function getTitles() {
   return getData("/titleInfos");
 }
 
-function getPayrollInfos() {
+function getPayrollInfos(year, no) {
+  if (!year && !no) {
     return getData("/payrollInfos");
+  } else {
+    let filter = {where: {}};
+    if (year) {
+      filter.where.year = year;
+    }
+    if (no && no !== "0") {
+      filter.where.no = no;
+    }
+    return getData("/payrollInfos", JSON.stringify(filter));
+  }
+
+
 }
 
 function getDepartmentalInfos() {
-    return getData("/departmentalInfos");
+  return getData("/departmentalInfos");
 
 }
 
-function getData(url) {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: `${host}/${url}`, type: "GET", success: function (data) {
-                resolve(data);
-            }, error: function (err) {
-                reject(err)
-            }
-        });
+function getData(url, condition) {
+  return new Promise((resolve, reject) => {
+    let r = `${host}${url}`;
+    if (condition) {
+      r += `?filter=${condition}`;
+    }
+    console.log(r);
+    $.ajax({
+      url: r, type: "GET", success: function (data) {
+        resolve(data);
+      }, error: function (err) {
+        reject(err)
+      }
     });
+  });
 }
